b9f74c50d23ffdd31210088cf16e300a4893a195
Samantha Izarraraz <80426961+Samwitch0322@users.noreply.github.com>
2021-06-22 09:55:08 -0500

    Tarjeta de credito
    Se añadio clase de tarjeta de credito

---
+++ Objects/CreditCard/CreditCard.cpp
@@ -0,0 +1,23 @@
+
+#include "CreditCard.hpp"
+
+
+CreditCard::CreditCard(float balanceCut, int creditLimit, string banco, int numeroPlastico, string varNIP)
+{
+    balanceAlCorte = balanceCut;
+    limiteCredito = creditLimit;
+}
+void CreditCard::setBalanceAlCorte(float balanceCut){
+    balanceAlCorte = balanceCut;
+}
+float CreditCard::getBalanceAlCorte(){
+    return balanceAlCorte;
+}
+void CreditCard::setLimiteCredito(int creditLimit){
+    limiteCredito = creditLimit;
+}
+int CreditCard::getlimiteCredito(){
+    return limiteCredito;
+}
+
+
---
+++ Objects/CreditCard/CreditCard.hpp
@@ -0,0 +1,24 @@
+#include<string>
+#include"../Card/Card.hpp"
+
+using namespace std;
+
+class CreditCard : public Card
+{
+    private:
+        float balanceAlCorte;
+        int limiteCredito;
+
+    public:
+        CreditCard(float balanceCut, int creditLimit, string banco, int numeroPlastico, string varNIP);
+
+
+        float getBalanceAlCorte();
+        int getlimiteCredito();
+        
+        void setBalanceAlCorte(float balanceCut);
+        void setLimiteCredito(int creditLimit);
+
+
+
+};
